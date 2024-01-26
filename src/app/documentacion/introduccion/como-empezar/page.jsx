c2ef75a7d763906659e5299d2c2f8662cf887eb2
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-01-25 18:08:51 -0600

    Se agrego mas informacion en seccion 'Como empezar'

--- src/app/documentacion/introduccion/como-empezar/page.jsx
+++ src/app/documentacion/introduccion/como-empezar/page.jsx
@@ -100,7 +100,28 @@ export default function ComoEmpezarPage(){
           
         </section>
         <section>
-          <h2 id='Creando nuestra Presentación'>Creando nuestra Presentación</h2>
+          <h2 id='Presentaciones'>Presentaciones</h2>
+          <p>
+          Como mencionaba anteriormente, las presentaciones son por llamarlo de alguna forma el archivo donde tenemos cargada y configurada el contenido que vamos a cargar al Player para ser proyectado.
+          </p>
+          <p>Antes de enseñarte a crearlo, te dejaré un diagrama para que entiendas un poco más cómo funciona la carga de contenido de forma global.
+          </p>
+          <img className="my-8 m-auto w-full" src="/img/bs-network/bs-diagram.png" alt="Diagrama de funcionamiento de BS" />
+          <p>Como se ve en el diagrama, nosotros creamos nuestra presentación, una vez creada, la subimos a nuestra nube en BS Network y una vez arroba, 
+          asignamos la presentación a los player donde queramos que BS Network mande el contenido.</p>
+          <br />
+          <h3>Sistema de archivos</h3>
+          <p>Las presentaciones se guardan en formato <Highlighted>.bpf </Highlighted>. Este formato no almacena más que referencia a los archivos que vas a cargar, esto quiere decir que tal cual 
+            la presentación no contiene las imágenes y los videos dentro de ella, sino que más bien, los archivos que vas a cargar a los players tienen que estar en la 
+            misma carpeta donde se encuentre la presentación.</p>
+          <img className="my-8 m-auto w-full" src="/img/bs-network/bs-diagram-file-sistem.png" alt="Diagrama de funcionamiento de BS" />
+          <p>En el ejemplo anterior vemos una forma recomendada de manejar tu estructura de archivos.</p>
+          
+          <DialogBox type="info" >
+            💡 Los formatos que se aceptan son variados dependiendo la versión del player para el que se creará la presentación. Los recomendados son MP3, MP4, PNG y JPG.
+          </DialogBox>
+
+          <h2 id="Creaundo una Presentación">Creaundo una Presentación</h2>
         </section>
 
         {/*----------------- Prefooter ------------- */}
