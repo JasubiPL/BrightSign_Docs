a9e589f158efc76469c021d5b564e18b148468cd
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-01-30 15:27:21 -0600

    Se añadio mas informacion a Crear presentacion

--- src/app/documentacion/introduccion/05-creando-presentacion/page.jsx
+++ src/app/documentacion/introduccion/05-creando-presentacion/page.jsx
@@ -2,6 +2,7 @@ import { DialogBox } from "@/components/DialogBox";
 import { NextPage } from "@/components/NextPage";
 import { PreviusPage } from "@/components/PreviusPage";
 import { Highlighted } from '@/components/Highlighted'
+import Link from "next/link";
 
 
 
@@ -50,6 +51,55 @@ export default function CreandoPresentacionPage(){
            💡 Recuerda que en la carpeta donde guardes tu presentación deberás guardar también tus Imágenes, videos y archivos que vayas a utilizar.
           </DialogBox>
 
+          <p>En la sección siguiente tenemos <Highlighted>BrightSign Model</Highlighted>, donde colocaremos entre la lista de opciones el modelo del Player que 
+          tenemos, debido a que los Players tienen diferentes características dependiendo el modelo.</p>
+          <p>El modelo viene en la parte inferior del player. En mi caso usare un <Highlighted>LS424</Highlighted> para este ejemplo.</p>
+
+          <img className="my-8 m-auto w-full lg:w-3/5" src="/img/players-models/pyr-bottom.png" alt="Modelo del player" />
+
+          <p>Después tenemos <Highlighted>Connector type</Highlighted>, aquí lo más probable es que sea HDMI, sin embargo, si pensamos conectar el Player por VGA o DVI, debemos cambiar esta opción.</p>
+          <p>La tercera opción es <Highlighted>Screen Resolution</Highlighted>, aquí de igual forma podemos seleccionar la resolución de nuestro contenido dependiendo la pantalla donde lo proyectaremos y los FPS.</p>
+          <p>Los Players de 5ta generación ya son capaces de soportar hasta 4k y 8k usando las versiones de BrightAuthor 5.0 en adelante.</p>
+
+          <DialogBox>
+            💡 En este caso, que estoy usando la versión 4.7.2.18, solo soporta players de hasta 4ta generación y presentaciones 1080p a 60fps
+          </DialogBox>
+
+          <p>Por último, tenemos <Highlighted>Monitor Orintation</Highlighted>, que es para poner la presentación en modo vertical en caso de requerirlo.</p>
+          <p>Una vez configurados todos los parámetros, presionaremos en <Highlighted>Create</Highlighted>.</p>
+          <p>Se nos desplegará una ventana donde nos dará una serie de opciones de plantillas predefinidas que podemos usar para acomodar nuestro contenido.</p>
+          <p>Seleccionemos <Highlighted>FullScreen</Highlighted> para este ejemplo. Recuerda que puedes encontrar todo sobre las plantillas y cómo crear las nuestras 
+          en la sección <Link className='link' href='/'>PLANTILLAS</Link> más adelante.</p>
+
+          <img className="my-8 m-auto w-full lg:w-3/5" src="/img/bs-author/bsa-layout-template-window.png" alt="Plantillas de presentacion" />
+
+          <p>Una vez haciendo esto, se nos abrirá el programa en la pestaña de <Highlighted>Edit</Highlighted>, aquí será donde agregaremos el contenido a la presentación, tanto al crear 
+            una nueva como al editar una existente.</p>
+          
+          <img className="my-8 m-auto w-full " src="/img/bs-author/bsa-edit-zones.png" alt="zonas de la pestaña edit" />
+
+          <p>En esta pestaña tenemos 3 zonas principales.</p>
+
+          <DialogBox type="comment">
+            <ul>
+              <li className="pb-3">
+                Imágenes y videos de la presentación 🖼️🎞️: <br />
+                <div className="ml-12 mb-2 mt-0">Aquí es la zona donde colocaremos las imágenes y videos que deseamos añadir a la presentación.</div>
+              </li>
+              
+              <li className="pb-3">
+                Raíz del proyecto 📂:
+                <div className="ml-6 mb-2 mt-0">Aquí están los elementos que tenemos en la carpeta donde guardamos la presentación.</div>
+              </li>
+              
+              <li className="pb-3">
+                Zona de plantilla 📐:
+                <div className="ml-6 mb-2 mt-0">Aquí podemos ver el área que ocupa y el tipo de contenido que recibe. En este caso solo añadiremos imagen y video y la 
+              etiqueta correspondiente es de color verde.</div>
+              </li>
+              
+            </ul>
+          </DialogBox>
         </section>
 
         {/*----------------- Prefooter ------------- */}
